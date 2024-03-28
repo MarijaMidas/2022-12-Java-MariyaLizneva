f598ef528a6480c78f2653b7d245508ef4d8eb51
Marija <marija.midas@gmail.com>
2024-03-28 14:28:50 +0300

    21/03/2024 hw14 fix to utf-8 and delete comment

--- hw14-SpringDataJDBC/src/main/java/app/WebServer.java
+++ hw14-SpringDataJDBC/src/main/java/app/WebServer.java
@@ -3,12 +3,6 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-    // Стартовая страница
-
-    http://localhost:8080/clients
-
-*/
 
 @SpringBootApplication
 public class WebServer {
